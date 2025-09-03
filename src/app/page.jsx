34b398f0664a1099158e2d741882d6b66122c840
Your Name <you@example.com>
2025-09-04 00:20:15 +0300

    my

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -15,7 +15,7 @@ const Homepage = () => {
     >
       <Cnsole />
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 p-20">
-        <div className="h-1/2 lg:h-full lg:w-5/12 flex justify-center items-center">
+        <div className="h-1/2 lg:h-full lg:w-5/12 flex justify-center items-center my-6 md:my-0">
           <div className="deneme"></div>
         </div>
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center text-white">
